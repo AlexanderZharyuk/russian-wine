27c22233874e7abc40a504627abf222574dfee02
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-06 12:16:56 +0300

    Упростил функцию получения продуктов из эксель файла

--- main.py
+++ main.py
@@ -7,25 +7,17 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def get_drinks_information(excel_table_name: str) -> defaultdict:
+def get_drinks(excel_table_name: str) -> defaultdict:
     """Get drinks information from excel table"""
-    drinks_info_table = pandas.read_excel(excel_table_name, sheet_name='Лист1', na_values='nan',
-                                          keep_default_na=False).values.tolist()
+    products = pandas.read_excel(excel_table_name, sheet_name='Лист1', na_values='nan',
+                                          keep_default_na=False).to_dict(orient='records')
 
-    drinks_information = [tuple(fields) for fields in drinks_info_table]
-    drinks = defaultdict(list)
+    grouped_products = defaultdict(list)
 
-    for drink_type, drink_name, drink_sort, drink_price, drink_image, drink_sale in drinks_information:
-        drink_info = {'Категория': drink_type,
-                      'Название': drink_name,
-                      'Сорт': drink_sort,
-                      'Цена': drink_price,
-                      'Картинка': drink_image,
-                      'Акция': drink_sale}
+    for product in products:
+        grouped_products[product['Категория']].append(product)
 
-        drinks[drink_type].append(drink_info)
-
-    return drinks
+    return grouped_products
 
 
 def get_excel_table_name() -> str:
@@ -44,7 +36,7 @@ def render_template() -> None:
     )
 
     excel_filename = get_excel_table_name()
-    drinks_info_from_excel = get_drinks_information(excel_filename)
+    drinks = get_drinks(excel_filename)
 
     current_year = datetime.now().year
     company_founding_date = 1920
@@ -52,7 +44,7 @@ def render_template() -> None:
 
     template = env.get_template('template.html')
     rendered_page = template.render(
-        company_age=company_age, drinks=drinks_info_from_excel
+        company_age=company_age, drinks=drinks
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
